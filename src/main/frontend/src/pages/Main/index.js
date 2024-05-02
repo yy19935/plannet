eb35acc96f5e2f9950c3e3860cf8c2a7a275113f
yunjeongLim <630yjl@gmail.com>
2024-05-02 22:02:39 +0900

    [FE]카카오로그인 수정

--- src/main/frontend/src/pages/Main/index.js
+++ src/main/frontend/src/pages/Main/index.js
@@ -1,28 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import React, { useEffect } from 'react';
 
 const Index = () => {
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    if (!mounted) {
+      handleKakaoRedirect();
+      setMounted(true);
+    }
+  }, [mounted]); // mounted 상태가 변경될 때만 useEffect 실행
 
   const handleKakaoRedirect = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
-    const requestURL = `http://localhost:8080/kakaoLogin?code=${code}`
+    const requestURL = `http://localhost:8080/kakaoLogin?code=${code}`;
 
     axios.get(requestURL)
-    .then((response) => {
-      // 요청 성공 시 실행할 코드
-      console.log('전송');
-    })
-    .catch((error) => {
-      // 요청 실패 시 실행할 코드
-      console.error(error);
-    });
-  }
+      .then((response) => {
+        // 요청 성공 시 실행할 코드
+        console.log('전송');
+      })
+      .catch((error) => {
+        // 요청 실패 시 실행할 코드
+        console.error(error);
+      });
+  };
 
   return (
     <div><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />메인 페이지</div>
   );
-}
+};
 
 export default Index;
