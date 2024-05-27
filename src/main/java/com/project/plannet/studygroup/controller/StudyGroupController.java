303cfa5c0db50d70d1283a1df9a2545108bfe3a2
kimjoohwan <yy19935@gmail.com>
2024-05-27 21:08:38 +0900

    [BE] 스터디그룹 생성 기능

--- src/main/java/com/project/plannet/studygroup/controller/StudyGroupController.java
+++ src/main/java/com/project/plannet/studygroup/controller/StudyGroupController.java
@@ -1,12 +1,12 @@
 package com.project.plannet.studygroup.controller;
 
+import com.project.plannet.member.vo.Member;
 import com.project.plannet.studygroup.service.StudyGroupService;
 import com.project.plannet.studygroup.vo.StudyGroup;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -21,5 +21,11 @@ public class StudyGroupController {
     @GetMapping("/studyGroup/list")
     public List<StudyGroup> getStudyGroupList(@RequestParam Map<String, String> param){
         return service.getStudyGroupList(param);
+    }
+
+    @PostMapping("/studyGroup/write")
+    public Map<String, Object> writeStudyGroup(@SessionAttribute(name = "loginMember", required = false) Member loginMember, @ModelAttribute StudyGroup studyGroup){
+        studyGroup.setMemberNo(loginMember.getMemberNo());
+        return service.saveStudyGroup(studyGroup);
     }
 }
