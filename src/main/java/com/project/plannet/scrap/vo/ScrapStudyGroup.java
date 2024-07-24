3696bfdba0acf509bdcbd6c885c292fb4f95740e
kimjoohwan <yy19935@gmail.com>
2024-07-25 02:56:51 +0900

    [BE] 스터디그룹 스크랩 기능 구현

---
+++ src/main/java/com/project/plannet/scrap/vo/ScrapStudyGroup.java
@@ -0,0 +1,13 @@
+package com.project.plannet.scrap.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ScrapStudyGroup {
+    int SG_NO;
+    int M_NO;
+}
